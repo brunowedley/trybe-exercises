23baf31c330435810a2534b4ffba4bb76fa34d92
Bruno Wedley <brunowedley27@gmail.com>
2020-10-23 18:37:53 -0300

    continuação da resolução das propostas de exercicios

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/item6.js
@@ -0,0 +1,10 @@
+let torre = ("cima, baixo, direita ,esquerda");
+let rainha = ("todas as direções em linhas retas");
+let rei = ("uma casa para todas as direções");
+let peão = ("pra frente");
+let bispo = ("diagonais");
+let cavalo = ("movimentação em L");
+
+"Qual peça de xadrez você gostaria de conhecer?"
+
+console.log(cavalo);
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/item8e9.js
@@ -0,0 +1,20 @@
+let a = 42;
+let b = 343;
+let c = 122;
+isEven = false;
+
+if (a%2 == 0 || b%2 == 0 || c%2 ==0 ){
+    isEven = true;
+    console.log (isEven)
+} else {
+    isEven = false;
+    console.log (isEven);
+}
+
+if (a%2 != 0 || b%2 != 0 || c%2 !=0 ){
+    isEven = true;
+    console.log (isEven)
+} else {
+    isEven = false;
+    console.log (isEven);
+}
