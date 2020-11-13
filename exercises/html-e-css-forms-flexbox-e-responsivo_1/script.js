1a7b91ee1b6f516592558fbf0723642e988befcc
Bruno Wedley <brunowedley27@gmail.com>
2020-11-13 13:39:40 -0300

    adição dos arquivos para exercicio

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,39 @@
+let statesObject = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+}
+for (let key in statesObject) {
+  let states = document.querySelector("#input-uf");
+  let option = document.createElement("option");
+  option.innerHTML = statesObject[key];
+  option.className = "states"
+  option.value = key;
+  states.appendChild(option);
+}
+
+let date = document.querySelector("#input-start-date");
