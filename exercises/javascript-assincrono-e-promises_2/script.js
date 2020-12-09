dca97f53b82a1ec4b1b273fac4302ec1f17f7143
Bruno Wedley <brunowedley27@gmail.com>
2020-12-09 19:06:09 -0300

    adiciona parte do exercicio do dia

---
+++ exercises/javascript-assincrono-e-promises_2/script.js
@@ -0,0 +1,16 @@
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const thisArray = Array.from(
+      { length: 10 }, () => Math.floor(Math.random() * 50) + 1
+    );
+    const sum = thisArray.map(number => number * number)
+      .reduce((acc, cur) => acc + cur, 0);
+    (sum < 8000) ? resolve() : reject();
+    console.log(sum);
+  });
+  myPromise
+    .then(() => console.log(`XABLEY =)`))
+    .catch(() => console.log(`XIBLEU =(`));
+};
+
+fetchPromise();
