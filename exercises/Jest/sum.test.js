69a8ad842fa568b0d33800d155c463642e26ef23
Bruno Wedley <brunowedley27@gmail.com>
2021-01-04 19:42:17 -0300

    adiciona exercicios

---
+++ exercises/Jest/sum.test.js
@@ -0,0 +1,23 @@
+const sum = require('./sum');
+
+describe('sum', () => {
+  it('4 + 5 = 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('throw an error when element is a string', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  it('0 + 0 = 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('error message is "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow(/parameters must be numbers/);
+  });
+});
