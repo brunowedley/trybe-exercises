2069d089ee16b83a5f0d58711eed99ef00c14952
Bruno Wedley <brunowedley27@gmail.com>
2021-01-04 18:35:37 -0300

    adiciona exercicios

--- exercises/javascript-assincrono-e-promises_2/script.js
+++ exercises/javascript-assincrono-e-promises_2/script.js
@@ -1,15 +1,16 @@
 const fetchPromise = () => {
   const myPromise = new Promise((resolve, reject) => {
     const thisArray = Array.from(
-      { length: 10 }, () => Math.floor(Math.random() * 50) + 1
+      { length: 10 },
+      () => Math.floor(Math.random() * 50) + 1
     );
     const sum = thisArray.map(number => number * number)
       .reduce((acc, cur) => acc + cur, 0);
-    (sum < 8000) ? resolve() : reject();
+    (sum < 8000) ? resolve(sum) : reject();
     console.log(sum);
   });
   myPromise
-    .then(() => console.log(`XABLEY =)`))
+    .then(sum => [2, 3, 5, 10].map(number => sum / number))
     .catch(() => console.log(`XIBLEU =(`));
 };
 
